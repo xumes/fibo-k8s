4f3f2fc06a379f86a356a2a74711ee39d40123a7
Reginaldo Santos <reginaldo.santos@redspace.com>
2022-03-03 11:33:15 -0400

    refactor: move to correct folder structure

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import About from './About';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/about" component={About} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
